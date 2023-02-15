c2b7bf5024481948c191b5c9972bb09eb0ca8ab3
kiranss777 <111719669+kiranss777@users.noreply.github.com>
2023-02-15 12:23:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I PES1UG20CS212_KIRANSS have successfully built and run\n";
-  cout << "Webhook added successfully";
   return 0;
 }
